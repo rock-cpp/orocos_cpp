c15fd3a2a713ea74f0bb9b499d30a1c7b87a5518
Alexander Duda <Alexander.Duda@dfki.de>
2010-11-29 14:38:57 +0100

    add config.sh

---
+++ config.sh
@@ -0,0 +1,147 @@
+#!/bin/sh
+
+# Author: Alexander Duda, Thomas Roehr, thomas.roehr@dfki.de
+# This script sets up a git repository based on an existing template project
+
+
+echo "Do you want to setup the cmake template"
+
+# Check and interprete answer of "Proceed [y|n]"
+ANSWER=""
+until [ "$ANSWER" = "y" ] || [ "$ANSWER" = "n" ] 
+do
+	echo "Proceed [y|n]"
+	read ANSWER
+	ANSWER=`echo $ANSWER | tr "[:upper:]" "[:lower:]"`
+done
+
+if [ "$ANSWER" = "n" ]
+	then echo "Aborted."
+	exit 0
+fi
+
+# Select package type
+PACKAGE_TYPE="CMAKE"
+
+# CMAKE-TEMPLATE-ADAPTION
+if [ $PACKAGE_TYPE = "CMAKE" ]; then
+	# Replace existance of dummyproject files
+	# TODO: do a more generic replacement  
+	mv configuration/dummyproject.pc.in configuration/$PACKAGE_SHORT_NAME.pc.in
+	mv configuration/dummyproject-core.properties configuration/$PACKAGE_SHORT_NAME-core.properties
+
+	# removing git references to prepare for new check in
+	find . | grep -e ".git$" | xargs rm -rf
+
+	# replace dummyproject with projectname
+	sed -i 's/dummyproject/'$PACKAGE_SHORT_NAME'/' CMakeLists.txt
+
+	PKG_DESC=
+	PKG_LONG_DESC=
+	PKG_AUTHOR=
+	PKG_EMAIL=
+	PKG_URL=
+	PKG_SCM=
+	PKG_SCM_URL=
+
+
+	# TODO 
+	PKG_LICENSE=
+	PKG_LOGO_URL=
+	PKG_DEPENDENCIES=
+	PKG_CFLAGS=
+	PKG_LFLAGS=
+
+	# File storing the package information
+	MANIFEST="manifest.xml"
+	echo "------------------------------------------"
+	echo "We require some information to update the $MANIFEST"
+	echo "------------------------------------------"
+	echo "Brief package description (Press ENTER when finished):"
+	read PKG_DESC
+
+	echo "Long description: "
+	read PKG_LONG_DESC
+	# REMOVE http:// since it is a bit messy to pass it through shell expansion
+	PKG_LONG_DESC=`echo $PKG_LONG_DESC | sed 's/http:\/\///g'`
+
+	echo "Author: "
+	read PKG_AUTHOR
+
+	echo "Author email: "
+	read PKG_EMAIL
+
+	echo "Url (optional):"
+	read PKG_URL
+	# REMOVE http:// since it is a bit messy to pass it through shell expansion
+	PKG_URL=`echo $PKG_URL | sed 's/http:\/\///g'`
+
+	# Yes, we only support git as SCM
+	PKG_SCM="git"
+	echo "Enter your dependencies as a comma separated list. Press ENTER when finished:"
+	read PKG_DEPENDENCIES
+
+	# But if there is svn or mercurial support ever
+	# we can already prompt the user ;)
+	#echo "Version control system [git|svn|mercurial]: " 
+	#echo 1: git
+	#echo 2: svn
+	#echo 3: mercury
+
+	#while [ 1 ]
+	#        do 
+	#	echo "Select from list:"
+	#	read PKG_SCM
+	#	case $PKG_SCM in
+	#	1) PKG_SCM="git"
+	#	   break
+	#	   ;;
+	#	2) PKG_SCM="svn"
+	#	   break
+	#	   ;;
+	#	3) PKG_SCM="mercury"
+	#           break
+	#	   ;;
+	#	esac
+	#done
+
+	sed -i "s/\(PROJECT_DESCRIPTION\ \).*/\1\"$PKG_DESC\")/" CMakeLists.txt
+	sed -i "s/dummy-brief-desc/$PKG_DESC/" $MANIFEST
+	sed -i "s/dummy-long-desc/$PKG_LONG_DESC/" $MANIFEST
+	sed -i "s/dummy-author/$PKG_AUTHOR/" $MANIFEST
+	sed -i "s/dummy-email/$PKG_EMAIL/" $MANIFEST
+	sed -i "s/dummy-url/http:\/\/$PKG_URL/" $MANIFEST
+	sed -i "s/dummy-version-control/$PKG_SCM/" $MANIFEST
+	sed -i "s/dummy-version-control-url/$PKG_SCM_URL/" $MANIFEST
+	sed -i "s/dummy-license/$PKG_LICENSE/" $MANIFEST
+	sed -i "s/dummy-logo-url/http:\/\/$PKG_LOGO_URL/" $MANIFEST
+
+	# Enter list of dependencies if there are any
+	if [ "$PKG_DEPENDENCIES" != "" ]
+		then
+		# Replace comma ','
+		PKG_DEPENDENCIES=`echo $PKG_DEPENDENCIES | sed "s/,/ /g"`
+		for dep in ${PKG_DEPENDENCIES}
+			do
+			# Use the <!--DEPEND-ENTRY--> as a hook for subsequent replacements
+			# replace with <depend package="pkgname"> 
+			sed -i "s/<\!--DEPEND-ENTRY-->/<depend package=\"${dep}\"\ \/>\n<\!--DEPEND-ENTRY-->/" $MANIFEST
+		done
+	fi
+
+	sed -i "s/dummy-cflags/$PKG_CFLAGS/" $MANIFEST
+	sed -i "s/dummy-lflags/$PKG_LFLAGS/" $MANIFEST
+fi
+# end of CMAKE-TEMPLATE-ADAPTION
+
+#delete setup script
+rm config.sh
+
+# creating a new git project --add adapted dummy-project contents and commit
+git init --shared=0770
+git add .
+git commit -m "Initial commit"
+
+
+echo "Done."
+
