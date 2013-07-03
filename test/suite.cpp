f473204cd28b9c02fd7eea2452c9dec284f61e5a
Jakob Schwendner <jakob.schwendner@dfki.de>
2013-07-03 10:02:49 +0200

    fixed typo

--- test/suite.cpp
+++ test/suite.cpp
@@ -1,5 +1,5 @@
 // Do NOT add anything to this file
 // This header from boost takes ages to compile, so we make sure it is compiled
-// only once (her)
+// only once (here)
 #define BOOST_TEST_MODULE dummy_project
 #include <boost/test/included/unit_test.hpp>
