7b46b73f0e4dc8d6077a7994e174608e62ddb0b8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-08-13 14:36:10 +0200

    since we link to the boost shared library, we don't have to use the 'included' method

--- test/suite.cpp
+++ test/suite.cpp
@@ -1,5 +1,6 @@
 // Do NOT add anything to this file
 // This header from boost takes ages to compile, so we make sure it is compiled
 // only once (here)
-#define BOOST_TEST_MODULE dummy_project
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
