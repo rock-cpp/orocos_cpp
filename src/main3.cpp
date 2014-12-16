0d0fb05bad0354cbff1c7c7b43eb2aef300be0f7
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-12-16 16:44:47 +0100

    fixed compile error / test case

--- src/main3.cpp
+++ src/main3.cpp
@@ -39,16 +39,9 @@ int main(int argc, char**argv)
     }
 
     ConfigurationHelper helper;
-    helper.loadConfigFile("config.yml");
-    
-    std::vector<std::string> names;
-    
-    names.push_back("default");
-    names.push_back("ground_based_sweeping");
-//     names.push_back("foo");
+    helper.setBundlePath("");
 
-
-    if(!helper.applyConfig(proxy, names))
+    if(!helper.applyConfig(proxy, "default", "ground_based_sweeping"))
         std::cout << "APPLY FAILED" << std::endl;
     
     return 0;
