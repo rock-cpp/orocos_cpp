6731c55bce436060c2071d85a85cfec950dcf2f7
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-02-20 16:48:15 +0100

    fixed transformation configuration
    Seems, that hasOperation is broken. It returns false,
    even if the operation exists.

--- src/TransformerHelper.cpp
+++ src/TransformerHelper.cpp
@@ -32,7 +32,7 @@ bool TransformerHelper::configureTransformer(RTT::TaskContext* task)
     const std::string opName("getNeededTransformations");
     
     //test if the task acutally uses the transformer
-    if(!task->provides()->hasOperation(opName))
+    if(!task->provides()->hasMember(opName))
         //does not, we take this as successfully configured
         return true;
     
