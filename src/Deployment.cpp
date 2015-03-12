96f1c7cc78326147704627c7624ec0c4c9f40764
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-03-12 11:43:04 +0100

    Added support for loading deployment information

---
+++ src/Deployment.cpp
@@ -0,0 +1,171 @@
+#include "Deployment.hpp"
+#include <boost/tokenizer.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <orocos_cpp_base/OrocosHelpers.hpp>
+
+Deployment::Deployment(const std::string& name) : deploymentName(name)
+{
+    loadPkgConfigFile(name);
+    if(!checkExecutable(name))
+        throw std::runtime_error("Error, executable for deployment " + name + " could not be found in PATH");
+}
+
+bool Deployment::checkExecutable(const std::string& name)
+{
+    if(boost::filesystem::exists(name))
+        return true;
+    
+    const char *binPath = getenv("PATH");
+    if(!binPath)
+    {
+        throw std::runtime_error("Internal Error, PATH is not set found.");
+    }
+    
+    std::string binPathS = binPath;
+    
+    boost::char_separator<char> sep(":");
+    boost::tokenizer<boost::char_separator<char> > paths(binPathS, sep);
+    
+    for(const std::string &path: paths)
+    {
+        std::string candidate = path + "/" + name;
+        if(boost::filesystem::exists(candidate))
+        {
+            return true;
+            break;
+        }
+    }
+
+    return false;
+}
+
+
+bool Deployment::loadPkgConfigFile(const std::string& name)
+{
+    std::vector<std::string> pkgConfigFields;
+    pkgConfigFields.push_back("typekits");
+    pkgConfigFields.push_back("deployed_tasks");
+    std::vector<std::string> pkgConfigValues;
+
+    if(!OrocosHelpers::parsePkgConfig("/orogen-" + name + ".pc", pkgConfigFields, pkgConfigValues))
+        throw std::runtime_error("Error, could not finde pkg-config file for deployment " + name );
+
+    
+    boost::char_separator<char> sep(" ");
+    boost::tokenizer<boost::char_separator<char> > tkits(pkgConfigValues[0], sep);
+    for(const std::string &tkit: tkits)
+        typekits.push_back(tkit);
+
+    boost::char_separator<char> sep2(",");
+    boost::tokenizer<boost::char_separator<char> > tTasks(pkgConfigValues[1], sep2);
+    for(const std::string &task: tTasks)
+    {
+        renameMap[task] = std::string();
+        originalTasks.push_back(task);
+        
+        std::string loggerString("_Logger");
+        if(task.length() > loggerString.length() && task.substr(task.length() - loggerString.length(), task.length()) == loggerString)
+        {
+            loggerName = task;
+        }
+    }
+
+//     std::cout << "Needed Typekits :" << std::endl;
+//     for(const std::string &tkit:  typekits)
+//     {
+//         std::cout << tkit << std::endl;
+//     }
+//     
+//     std::cout << "Deployed Tasks :" << std::endl;
+//     for(const std::string &task: originalTasks)
+//     {
+//         std::cout << task << std::endl;
+//     }
+//     
+    return true;
+}
+
+const std::string& Deployment::getName()
+{
+    return deploymentName;
+}
+
+const std::vector< std::string >& Deployment::getOriginalTaskNames() const
+{
+    return originalTasks;
+}
+
+
+const std::vector< std::string >& Deployment::getTaskNames() const
+{
+    return tasks;
+}
+
+const std::vector< std::string >& Deployment::getNeededTypekits() const
+{
+    return typekits;
+}
+
+void Deployment::renameTask(const std::string& orignalName, const std::string& newName)
+{
+    auto it = renameMap.find(orignalName);
+    if(it == renameMap.end())
+    {
+        throw std::runtime_error("Error, deployment " + deploymentName + " has no task " + orignalName);
+    }
+    
+    if(!it->second.empty())
+    {
+        std::cout << "Warning double renaming of task " << orignalName << ". This smells like a bug." << std::endl;
+    }
+    
+    //set new name
+    it->second = newName;
+
+    //regenerate the task list
+    tasks.clear();
+    for(std::pair<std::string, std::string> p: renameMap)
+    {
+        if(p.second.empty())
+        {
+            tasks.push_back(p.first);
+        }
+        else
+        {
+            tasks.push_back(p.second);
+        }
+    }
+}
+
+
+bool Deployment::getExecString(std::string& cmd, std::vector< std::string >& args)
+{
+    cmd = deploymentName;
+    
+    args.clear();
+    for(std::pair<std::string, std::string> p: renameMap)
+    {
+        if(!p.second.empty())
+        {
+            std::cout << "Task " << p.first << " is renmaed to " << p.second << std::endl;
+            args.push_back("--rename");
+            args.push_back(p.first + ":" + p.second);
+        }
+    }
+    
+    return true;
+}
+
+const std::string Deployment::getLoggerName() const
+{
+    auto it = renameMap.find(loggerName);
+    if(it == renameMap.end())
+        throw std::runtime_error("Internal Error, logger name could not be found");
+    return it->second;
+}
+
+bool Deployment::hasLogger() const
+{
+    return !loggerName.empty();
+}
---
+++ src/Deployment.hpp
@@ -0,0 +1,69 @@
+#ifndef DEPLOYMENT_H
+#define DEPLOYMENT_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <boost/noncopyable.hpp>
+
+class Deployment : public boost::noncopyable
+{
+private:
+    bool loadPkgConfigFile(const std::string &name);
+    bool checkExecutable(const std::string &name);
+    
+    std::vector<std::string> typekits;
+    std::vector<std::string> tasks;
+    std::vector<std::string> originalTasks;
+
+    std::vector<std::string> renamedTasks;
+    
+    std::map<std::string, std::string> renameMap;
+    
+    std::string deploymentName;
+    std::string loggerName;
+
+public:
+    Deployment(const std::string &name);
+    
+    /**
+     * Returns the name of the deployment
+     * */
+    const std::string &getName();
+    
+    /**
+     * Returns the task names, after renaming.
+     * */
+    const std::vector<std::string> &getTaskNames() const;
+    
+    /**
+     * Returns the task names before the renaming operation
+     * */
+    const std::vector<std::string> &getOriginalTaskNames() const;
+    const std::vector<std::string> &getNeededTypekits() const;
+    
+    /**
+     * Renames a task from the deployment to the given name
+     * */
+    void renameTask(const std::string &orignalName, const std::string &newName);
+
+    /**
+     * Returns the command line string, needed to 
+     * start the deployment, including the renaming 
+     * operations.
+     * */
+    bool getExecString(std::string& cmd, std::vector< std::string >& args);
+    
+    /**
+     * Returns the name of the logger for this deployment
+     * */
+    const std::string getLoggerName() const;
+    
+    /**
+     * Returns if this deployment contains a logger
+     * */
+    bool hasLogger() const;
+    
+};
+
+#endif // DEPLOYMENT_H
