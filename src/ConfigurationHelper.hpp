b392790cdfb570f8f9031d28f027299ce7b4857b
Elmar Berghöfer <elmar.berghoefer@dfki.de>
2015-09-29 13:24:08 +0200

    Function description added.

--- src/ConfigurationHelper.hpp
+++ src/ConfigurationHelper.hpp
@@ -77,6 +77,17 @@ public:
      */
     bool applyConfigString(RTT::TaskContext *context, const std::string &configYamlString);
 
+    /**
+     * This function analyses the given string and replaces variables.
+     * The variables in the string taht will be evaluated are given by "<%= ENV('xyz') %>" or
+     * "<%= BUNDLES('xyz') %>". In the case of ENV, the given value xyz will be treeted as an
+     * environment variable that will be replaced by its current value. BUNDLES specifies that xyz in that case is a
+     * relative path (relative with respect to a bundle root). This path will be converted in an absolute path
+     * by adding the prefix path of the bundle in which the path was found (starting with the default bundle if specified).
+     *
+     * @param val Is a const string reference parameter, which contains the original string with the variables.
+     * @returns String containing the enhanced string with all replacements.
+     */
     static std::string applyStringVariableInsertions(const std::string &val);
 private:
     /**
