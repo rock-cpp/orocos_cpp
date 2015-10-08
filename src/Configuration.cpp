c921c2ec838001d6ab9262984b84754484a4eded
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-10-08 12:27:03 +0200

    implemented Configuration::fillFromYaml

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -227,7 +227,12 @@ const std::string& Configuration::getName() const
     return name;
 }
 
-
+bool Configuration::fillFromYaml(const std::string& yml)
+{
+    values.clear();
+    YAMLConfigParser parser;
+    return parser.parseYAML(*this, parser.applyStringVariableInsertions(yml));
+}
 
 bool Configuration::merge(const Configuration& other)
 {
