0016f053bb21514153a11867635653219b916cce
Dennis Hemker <dhem2012@informatik.uni-bremen.de>
2017-04-26 15:48:01 +0200

    made typelist loading generic via pkg-config files

--- src/TypeRegistry.cpp
+++ src/TypeRegistry.cpp
@@ -1,5 +1,7 @@
 #include "TypeRegistry.hpp"
+#include "PkgConfigHelper.hpp"
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
 #include <iostream>
 #include <fstream>
 
@@ -16,49 +18,70 @@ TypeRegistry::TypeRegistry()
 
 bool TypeRegistry::loadTypelist()
 {
-    const char *pathsC = getenv("ROCK_PREFIX");
-    if(!pathsC)
+    const char *pkgCfgPaths = getenv("PKG_CONFIG_PATH");
+    
+    if(!pkgCfgPaths)
     {
         return false;
     }
+    
+    std::vector<std::string> paths;
+    boost::split(paths, pkgCfgPaths, boost::is_any_of(":"));
 
-    boost::filesystem::path orogenPath(std::string(pathsC) + "/../orogen");
-    
-    std::string ending(".typelist");
-    
-    for(auto it = boost::filesystem::directory_iterator(orogenPath); it != boost::filesystem::directory_iterator(); it++)
+    for(const std::string& path : paths)
     {
-        const auto file = it->path();
-        std::string f = file.filename().string();
-        size_t s = f.size();
-        if(s < ending.size())
-            continue;
-        
-        if(f.substr(s-ending.size(), s) == ending)
+        for(auto it = boost::filesystem::directory_iterator(path); it != boost::filesystem::directory_iterator(); it++)
         {
-            std::string typeKitName = f.substr(0, s-ending.size());
-//             std::cout << "TypeKit is " << typeKitName << std::endl;
-        
-            std::ifstream in(it->path().string());
-            if(in.bad())
-            {
-                throw std::runtime_error("Failed to open file " + f);
+            const boost::filesystem::path file = it->path();
+            if(file.filename().string().find("typekit") != std::string::npos)
+            {                
+                // be aware of order of parsed fields
+                std::vector<std::string> result, fields{"prefix", "project_name", "type_registry"};
+                if(PkgConfigHelper::parsePkgConfig(file.filename().string(), fields, result))
+                {
+                    if(PkgConfigHelper::solveString(result.at(2), "${prefix}", result.at(0)))
+                    {
+                        std::string typeKitPath = result.at(2);
+                        boost::replace_last(typeKitPath, "tlb", "typelist");
+                        
+                        std::ifstream in(typeKitPath);
+                        if(in.bad())
+                        {
+                            throw std::runtime_error("Failed to open file " + file.filename().string());
+                        }
+                        
+                        std::string line;
+                        while(!in.eof())
+                        {
+                            std::getline(in, line);
+                            size_t e = line.find_first_of(' ');
+                            if(e == std::string::npos)
+                                continue;
+                            
+                            std::string typeKitName = result.at(1);
+                            std::string typeName = line.substr(0, e);
+//                             std::cout << "Adding " <<  typeName << " to TK " << typeKitName << std::endl;
+                            
+                            typeToTypekit.insert(std::make_pair(typeName, typeKitName));
+                        }
+                        
+                    }
+                    else
+                    {
+                        std::cerr << "error: couldn't solve pkg-config strings from file " << file.string() << std::endl;
+                        return false;
+                    }
+                }
+                else
+                {
+                    std::cerr << "error: couldn't parse pkg-config fields from file " << file.string() << std::endl;
+                    return false;
+                }
+                
             }
             
-            std::string line;
-            while(!in.eof())
-            {
-                std::getline(in, line);
-                size_t e = line.find_first_of(' ');
-                if(e == std::string::npos)
-                    continue;
-
-                std::string typeName = line.substr(0, e);
-//                 std::cout << "Adding " <<  typeName << " to TK " << typeKitName << std::endl;
-                
-                typeToTypekit.insert(std::make_pair(typeName, typeKitName));
-            }
         }
+        
     }
     
     return true;
