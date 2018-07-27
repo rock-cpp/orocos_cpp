9db0096e7111b847fed18e834e1ee59217b70212
Sascha Arnold <sascha.arnold@dfki.de>
2018-07-27 14:37:09 +0200

    added an example interface for the improved version of this class
    Tlb files shall be parsed using the typelib library
    It shall be possible to determine the ID of a given task state name

--- src/TypeRegistry.hpp
+++ src/TypeRegistry.hpp
@@ -3,18 +3,43 @@
 #include <string>
 #include <map>
 
+namespace Typelib
+{
+    class Registry;
+}
+
 namespace orocos_cpp
 {
 
 class TypeRegistry
 {
     std::map<std::string, std::string> typeToTypekit;
+    std::map<std::string, unsigned> taskStateToID;
 public:
     TypeRegistry();
     
-    bool loadTypelist();
+    /**
+     * Laods all type registries in order to receive the nessesary informations.
+     */
+    bool loadTypeRegistries();
     
     bool getTypekitDefiningType(const std::string &typeName, std::string &typekitName);
+
+    /**
+     * Returns the ID of a given state name of a task.
+     * @param task_model_name e.g. "auv_control::AccelerationController"
+     * @param state_name e.g. "CONTROLLING"
+     * @param id the corresponding state ID
+     *
+     * @returns false if the state or the task is unknown
+     */
+    bool getStateID(const std::string &task_model_name, std::string &state_name, unsigned& id) const;
+
+protected:
+    /**
+     * Loads the registry of a given task model name.
+     */
+    bool loadRegistry(const std::string &task_model_name, Typelib::Registry* registry);
 };
 
 }
