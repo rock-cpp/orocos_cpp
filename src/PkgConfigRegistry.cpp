256aad1391a9de90d7d2166d2b1fc185bf1b46d3
Malte Wirkus <malte.wirkus@dfki.de>
2019-07-19 17:28:27 +0200

    Added PkgConfigRegistry

---
+++ src/PkgConfigRegistry.cpp
@@ -0,0 +1,344 @@
+#include "PkgConfigRegistry.hpp"
+#include "PkgConfigHelper.hpp"
+#include <regex>
+#include <boost/filesystem.hpp>
+#include <base-logging/Logging.hpp>
+
+orocos_cpp::PkgConfigRegistryPtr __pkgcfgreg = nullptr;
+
+template<typename T>
+void extract_keys(const std::map<std::string,T>& m, std::vector<std::string>& res){
+    for(const auto& kv : m) {
+        res.push_back(kv.first);
+    }
+}
+
+orocos_cpp::PkgConfig::PkgConfig() :
+    sourceFile("")
+{
+
+}
+
+bool orocos_cpp::PkgConfig::getVariable(const std::string &fieldName, std::string& value)
+{
+    std::map<std::string, std::string>::iterator it = variables.find(fieldName);
+    if(it == variables.end())
+        return false;
+
+    value = it->second;
+    return true;
+}
+
+bool orocos_cpp::PkgConfig::getProperty(const std::string &fieldName, std::string& value)
+{
+    std::map<std::string, std::string>::iterator it = properties.find(fieldName);
+    if(it == properties.end())
+        return false;
+
+    value = it->second;
+    return true;
+}
+
+bool orocos_cpp::PkgConfig::load(const std::string &filepath)
+{
+    bool st = PkgConfigHelper::parsePkgConfig(filepath, variables, properties);
+    bool got_name = getProperty("Name", name);
+    sourceFile = filepath;
+    return st;
+}
+
+bool orocos_cpp::PkgConfig::isLoaded()
+{
+    return !this->sourceFile.empty();
+}
+
+
+bool orocos_cpp::PkgConfigRegistry::initialize()
+{
+    LOG_INFO_S << "Scanning for PKGConfig files for orogen project, typekits, transports, tasks and deployments";
+    std::vector<std::string> searchPaths = PkgConfigHelper::getSearchPathsFromEnvVar();
+    scan(searchPaths);
+    LOG_INFO_S << "Scanning for PKG config files done.";
+    return true;
+}
+
+bool orocos_cpp::PkgConfigRegistry::getDeployment(const std::string &name, orocos_cpp::PkgConfig &pkg)
+{
+    std::map<std::string, PkgConfig>::iterator it = deployments.find(name);
+    if(it == deployments.end()){
+        return false;
+    }
+    pkg = it->second;
+    return true;
+}
+
+bool orocos_cpp::PkgConfigRegistry::getTypekit(const std::string &name, orocos_cpp::TypekitPkgConfig &pkg)
+{
+    std::map<std::string, TypekitPkgConfig>::iterator it = typekits.find(name);
+    if(it == typekits.end()){
+        return false;
+    }
+    pkg = it->second;
+    return true;
+}
+
+bool orocos_cpp::PkgConfigRegistry::getOrogen(const std::string &name, orocos_cpp::OrogenPkgConfig &pkg)
+{
+    std::map<std::string, OrogenPkgConfig>::iterator it = orogen.find(name);
+    if(it == orogen.end()){
+        return false;
+    }
+    pkg = it->second;
+    return true;
+}
+
+std::vector<std::string> orocos_cpp::PkgConfigRegistry::getRegisteredDeploymentNames()
+{
+    std::vector<std::string> ret;
+    extract_keys(deployments, ret);
+    return ret;
+}
+
+std::vector<std::string> orocos_cpp::PkgConfigRegistry::getRegisteredTypekitNames()
+{
+    std::vector<std::string> ret;
+    extract_keys(typekits, ret);
+    return ret;
+}
+
+std::vector<std::string> orocos_cpp::PkgConfigRegistry::getRegisteredOrogenNames()
+{
+    std::vector<std::string> ret;
+    extract_keys(orogen, ret);
+    return ret;
+}
+
+orocos_cpp::PkgConfigRegistryPtr orocos_cpp::PkgConfigRegistry::get()
+{
+    if(!__pkgcfgreg){
+        __pkgcfgreg = PkgConfigRegistryPtr(new PkgConfigRegistry());
+        __pkgcfgreg->initialize();
+    }
+    return __pkgcfgreg;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isTransportPkg(const std::string &filename, std::string &typekitName, std::string &transportName, std::string& arch)
+{
+    std::regex r(R"(([\w\d]+)-transport-([\w]+)-([\w]+).pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is Transport:";
+        typekitName = match[1];
+        transportName = match[2];
+        arch = match[3];
+        LOG_DEBUG_S << "  typekit: "<<typekitName<<"\n  trasnport: "<<transportName<<"\n  arch: "<<arch;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isOrogenTasksPkg(const std::string &filename, std::string &orogenProjectName, std::string& arch)
+{
+    std::regex r(R"(([\w\d]+)-tasks-([\w\d]+).pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is OrogenTasks:";
+        orogenProjectName = match[1];
+        arch = match[2];
+        LOG_DEBUG_S << "  orogen: "<<orogenProjectName<<"\n  arch: "<<arch;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isOrogenProjectPkg(const std::string &filename, std::string &orogenProjectName)
+{
+    std::regex r(R"(orogen-project-([\w\d]+).pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is OrogenProject:";
+        orogenProjectName = match[1];
+        LOG_DEBUG_S << "  orogen: "<<orogenProjectName;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isTypekitPkg(const std::string &filename, std::string &typekitName, std::string& arch)
+{
+    std::regex r(R"(([\w\d]+)-typekit-([\w\d]+).pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is Typekit:";
+        typekitName = match[1];
+        arch = match[2];
+        LOG_DEBUG_S << "  typekit: "<<typekitName;
+        LOG_DEBUG_S << "  arch: "<<arch;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isProxiesPkg(const std::string &filename, std::string &orogenProjectName)
+{
+    std::regex r(R"(([\w\d]+)-proxies.pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is Proxies:";
+        orogenProjectName = match[1];
+        LOG_DEBUG_S << "  orogen: "<<orogenProjectName;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::isDeploymentPkg(const std::string &filename, std::string &deploymentName)
+{
+    std::regex r(R"(orogen-([\w\d]+).pc)");
+    std::smatch match;
+    if(std::regex_match(filename, match, r)){
+        LOG_DEBUG_S << filename << " is Deployment:";
+        deploymentName = match[1];
+        LOG_DEBUG_S << "  depoyment: "<<deploymentName;
+        return true;
+    }
+    return false;
+}
+
+bool orocos_cpp::PkgConfigRegistry::addFile(const std::string &filepath)
+{
+    std::string typekitName, orogenProjectName, deploymentName, arch, transportName;
+    PkgConfig pkg;
+    boost::filesystem::path p(filepath);
+    std::string filename = p.filename().string();
+
+    //Is Deployment?
+    if(isDeploymentPkg(filename, deploymentName)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, PkgConfig>::iterator it = deployments.find(deploymentName);
+        if(it != deployments.end()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes a deployment with name " << deploymentName << ", but there was already a PKGConfig file for the same deployment added with the file " << it->second.sourceFile << ".";
+            return false;
+        }
+        deployments[deploymentName] = pkg;
+        return st;
+    }
+
+    //Is Proxy?
+    else if(isProxiesPkg(filename, orogenProjectName)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, OrogenPkgConfig>::iterator it = orogen.find(orogenProjectName);
+
+        if(it == orogen.end()){
+            orogen[orogenProjectName] = OrogenPkgConfig();
+            it = orogen.find(orogenProjectName);
+        }
+
+        if(it->second.proxies.isLoaded()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes task proxies for the orogen project " << orogenProjectName << ", but they were already be imported from the PKGConfig file " << it->second.proxies.sourceFile << ".";
+            return false;
+        }
+        it->second.proxies = pkg;
+        return st;
+    }
+
+    //Is OrogenProject
+    else if(isOrogenProjectPkg(filename, orogenProjectName)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, OrogenPkgConfig>::iterator it = orogen.find(orogenProjectName);
+
+        if(it == orogen.end()){
+            orogen[orogenProjectName] = OrogenPkgConfig();
+            it = orogen.find(orogenProjectName);
+        }
+
+        if(it->second.project.isLoaded()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes the orogen project " << orogenProjectName << ", but it was already described by the PKGConfig file " << it->second.project.sourceFile << ".";
+            return false;
+        }
+        it->second.project = pkg;
+        return st;
+    }
+
+    //Is OrogenTasks
+    else if(isOrogenTasksPkg(filename, orogenProjectName, arch)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, OrogenPkgConfig>::iterator it = orogen.find(orogenProjectName);
+
+        if(it == orogen.end()){
+            orogen[orogenProjectName] = OrogenPkgConfig();
+            it = orogen.find(orogenProjectName);
+        }
+
+        if(it->second.tasks.isLoaded()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes the tasks for the orogen project " << orogenProjectName << ", but the tasks were already described by the PKGConfig file " << it->second.tasks.sourceFile << ".";
+            return false;
+        }
+        it->second.tasks = pkg;
+        return st;
+    }
+
+    //Is Transports
+    else if(isTransportPkg(filename, typekitName, transportName, arch)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, TypekitPkgConfig>::iterator it = typekits.find(typekitName);
+
+        if(it == typekits.end()){
+            typekits[typekitName] = TypekitPkgConfig();
+            it = typekits.find(typekitName);
+        }
+
+        std::map<std::string, PkgConfig>::iterator transportit = it->second.transports.find(transportName);
+        if(transportit != it->second.transports.end()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes the transport " << transportName << " for typekit " << typekitName << ", but the transport was already described by the PKGConfig file " << transportit->second.sourceFile << ".";
+            return false;
+        }
+        it->second.transports[transportName] = pkg;
+        return st;
+    }
+
+    //Is Typekit
+    else if(isTypekitPkg(filename, typekitName, arch)){
+        bool st = pkg.load(filepath);
+        std::map<std::string, TypekitPkgConfig>::iterator it = typekits.find(typekitName);
+        if(it == typekits.end()){
+            typekits[typekitName] = TypekitPkgConfig();
+            it = typekits.find(typekitName);
+        }
+
+        if(it->second.typekit.isLoaded()){
+            LOG_WARN_S << "Ignoring PKGConfig file "<<filepath<<", because it describes the typekit " << typekitName << ", but the typekit was already described by the PKGConfig file " << it->second.typekit.sourceFile << ".";
+            return false;
+        }
+        it->second.typekit = pkg;
+        return st;
+    }
+    else{
+        //Do nothing.. we don't load pkgconfig files for arbitrary libraries, since we'll not need them.
+        return false;
+    }
+}
+
+void orocos_cpp::PkgConfigRegistry::scan(const std::vector<std::string> &searchPaths)
+{
+    namespace fs = boost::filesystem;
+
+    for(const std::string& path : searchPaths){
+        if(!fs::is_directory(path)){
+            LOG_WARN_S << "Skipping directory " << path << " since it is not a valid directory";
+            continue;
+        }
+        for (fs::directory_iterator itr(path); itr!=fs::directory_iterator(); ++itr)
+        {
+            if( fs::is_regular_file(*itr) ){
+                bool st = addFile(itr->path().string());
+                if(st){
+                    LOG_INFO_S << "Loaded PkgConfig file " << itr->path().string() << "\t" << "[OK]";
+                }else{
+                    LOG_INFO_S << "Loaded PkgConfig file " << itr->path().string() << "\t" << "[IGNORED]";
+                }
+            }
+        }
+    }
+}
+
---
+++ src/PkgConfigRegistry.hpp
@@ -0,0 +1,106 @@
+#include <map>
+#include <vector>
+#include <memory>
+
+namespace orocos_cpp{
+class PkgConfig
+{
+public:
+    PkgConfig();
+    /*!
+     * \brief Get value of a variable defined within PKGConfig
+     * Varibales in PKGConfig files can have arbitrary names. They are assigned by "varname=value".
+     * Example:
+     *   prefix=/my/prefix
+     *   exec_prefix=${prefix}
+     *   libdir=${prefix}/lib/orocos/types
+     * \param variableName : name of the variable
+     * \param value : assigned value of the variable \p variableName will be be retured here
+     * \return true of value was successfully read. False if variable was not defined and thus value could net be determined.
+     */
+    bool getVariable(const std::string& variableName, std::string& value);
+
+    /*!
+     * \brief Get value of a property defined within PKGConfig
+     * In PKGConfig files there exists a defined set of properties to describe a software module for compiling and linking. They are assigned by "propname: value".
+     *   Example:
+     *   Name: testTypekit
+     *   Version: 0.0
+     *   Requires: test
+     *   Description: test types support for the Orocos type system
+     *   Libs: -L${libdir} -ltest-typekit-gnulinux
+     *   Cflags: -I${includedir} -I${includedir}/test/types "-DOROCOS_TARGET=gnulinux"
+     * \param propertyName : name of the property
+     * \param value : assigned value of the property \p propertyName will be be retured here
+     * \return true of value was successfully read. False if varibale was not defined and thus value could net be determined.
+     */
+    bool getProperty(const std::string& propertyName, std::string& value);
+
+    /*!
+     * \brief load and parse a PKGConfig file
+     * The file will be searched in paths specified in the PKG_CONFIG_PATH envirionment variable
+     * \param filename
+     */
+    bool load(const std::string& filename);
+    std::string name;
+    std::string sourceFile;
+
+    /*!
+     * \brief Checks wether PKGConfig file has been loaded
+     * \return
+     */
+    bool isLoaded();
+
+protected:
+    std::map<std::string, std::string> variables;
+    std::map<std::string, std::string> properties;
+};
+
+class TypekitPkgConfig{
+public:
+    PkgConfig typekit;
+    //! Maps transport name (e.g. 'mqueue') to the corresponding PKGConfig
+    std::map<std::string, PkgConfig> transports;
+};
+
+class OrogenPkgConfig{
+public:
+    PkgConfig tasks;
+    PkgConfig project;
+    PkgConfig proxies;
+};
+
+class PkgConfigRegistry;
+typedef std::shared_ptr<PkgConfigRegistry> PkgConfigRegistryPtr;
+PkgConfigRegistryPtr __pkgcfgreg;
+
+class PkgConfigRegistry
+{
+public:
+    bool initialize();
+    bool getDeployment(const std::string& name, PkgConfig& pkg);
+    bool getTypekit(const std::string& name, TypekitPkgConfig &pkg);
+    bool getOrogen(const std::string& name, OrogenPkgConfig& pkg);
+    std::vector<std::string> getRegisteredDeploymentNames();
+    std::vector<std::string> getRegisteredTypekitNames();
+    std::vector<std::string> getRegisteredOrogenNames();
+    static PkgConfigRegistryPtr get();
+
+protected:
+    //! To what kind of package a Pkgconfig file is related to is determined by its filename (NOT path, must be a file name!)
+    bool isTransportPkg(const std::string& filename, std::string& typekitName, std::string& transportName, std::string &arch);
+    bool isOrogenTasksPkg(const std::string& filename, std::string& orogenProjectName, std::string &arch);
+    bool isOrogenProjectPkg(const std::string& filename, std::string& orogenProjectName);
+    bool isTypekitPkg(const std::string& filename, std::string& typekitName, std::string &arch);
+    bool isProxiesPkg(const std::string& filename, std::string& orogenProjectName);
+    bool isDeploymentPkg(const std::string& filename, std::string& deploymentName);
+
+    bool addFile(const std::string& filepath);
+    void scan(const std::vector<std::string>& searchPaths);
+
+    std::map<std::string, PkgConfig> deployments;
+    std::map<std::string, OrogenPkgConfig> orogen;
+    std::map<std::string, TypekitPkgConfig> typekits;
+};
+
+}
